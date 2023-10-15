32b9cfecefbc30eb30c8148be89638c3ed0423e9
Akkusativ23 <s6arweis@uni-bonn.de>
2023-10-15 17:01:41 +0200

    docker added

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
